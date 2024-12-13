a65d4dd4279c4cf443c28242c1ae491d3f4849fd
Don Nimesh <donnimesh60@gmail.com>
2024-12-14 00:09:26 +0100

    completed the models

---
+++ controllers/menucontroller.go
@@ -0,0 +1,40 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+
+
+
+func GetMenus() gin.HandlerFunc{
+	return func(c *gin.Context){
+
+	}
+
+
+}
+
+func GetMenu() gin.HandlerFunc{
+	return func(c *gin.Context){
+		
+	}
+
+
+}
+
+func CreateMenus() gin.HandlerFunc{
+	return func(c *gin.Context){
+		
+	}
+
+
+}
+
+
+
+func UpdateMenu() gin.HandlerFunc{
+	return func(c *gin.Context){
+		
+	}
+
+
+}
